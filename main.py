e2eded8eb57b0de88c621cf2cecd33844afb4fdb
Oluwashogo7 <105356237+Oluwashogo7@users.noreply.github.com>
2022-05-28 11:42:45 +0100

    Reading text files

--- main.py
+++ main.py
@@ -1,39 +1,26 @@
-class Student:
-    # [assignment] Skeleton class. Add your code here
-    def __init__(self, name, age, track, score):
-        self.name = name
-        self.age = age
-        self.track = track
-        self.score = score
+# Read text from a file, and count the occurence of words in that text
+# Example:
+# count_words("The cake is done. It is a big cake!") 
+# --> {"cake":2, "big":1, "is":2, "the":1, "a":1, "it":1}
 
-    def change_name(self, new_name):
-        self.name = new_name
-        return self.name
+def read_file_content(filename):
+    # [assignment] Add your code here 
+    with open(filename) as f:
+        lines = f.read()
+    return lines
     
-    def change_age(self, new_age):
-        self.age = new_age
-        return self.age
+                   
+def count_words():
+    text = read_file_content("./story.txt")
+    # [assignment] Add your code here
+    counter = {}
+
+    for word in text.split():
+        word = word.lower()
+        if word not in counter.keys():
+            counter[word] = 1
+        else:
+            counter[word] += 1
     
-    def add_track(self, new_track):
-        self.track = self.track.append(new_track)
-        return self.track
-    
-    def get_score(self):
-        return self.score
-
-
-            
-Bob = Student(name= "Bob", age = 26, track = ["FE","BE"], score = 20.90)
-
-# Expected methods
-Bob.change_name("Peter")
-print(Bob.name)
-
-Bob.change_age(34)
-print(Bob.age)
-
-Bob.add_track("UI/UX")
-print(Bob.track)
-
-Bob.get_score()
-print(Bob.get_score())
+    return counter
+print(count_words())
